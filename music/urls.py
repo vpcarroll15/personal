38b766a4c4a8252f3d5215e1e353c56605c2a205
Paul Carroll <vpcarroll15@gmail.com>
2019-04-07 18:57:46 -0700

    Refactor everything to use HTML templates.

--- music/urls.py
+++ music/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'music'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('music/<int:music_id>', views.music, name="music_detailed")
+    path('music/<int:music_id>', views.music, name="music_detailed"),
+    path('search', views.search, name="search"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, render
+from django.db.models import Q
 
 from .models import Music
 
@@ -6,12 +7,12 @@ from .models import Music
 def home(request):
     """Renders the homepage for the music app."""
     recent_music = Music.objects.order_by('-created_at')[:10]
-    context = {'recent_music': recent_music}
+    context = {'albums': recent_music}
 
     recommended_albums = Music.objects.filter(rating__gte=2)
     if recommended_albums:
         album_of_the_month = recommended_albums.order_by('-created_at')[0]
-        context['album_of_the_month'] = album_of_the_month
+        context['album'] = album_of_the_month
 
     return render(request, 'music/home.html', context)
 
@@ -21,5 +22,20 @@ def music(request, music_id):
     music = get_object_or_404(Music, pk=music_id)
     tags = music.musician.tags.all()
 
-    context = {'music': music, 'tags': tags}
+    context = {'album': music, 'tags': tags}
     return render(request, 'music/music.html', context)
+
+
+def search(request):
+    """Searches for a particular search term in our set of musicians, music, and tags."""
+    search_term = request.GET['search_term']
+
+    # TODO: Make this case-insensitive.
+    music = Music.objects.filter(
+        Q(name__startswith=search_term) |
+        Q(musician__name__startswith=search_term) |
+        Q(musician__tags__name__startswith=search_term)
+    )
+
+    context = {'albums': music }
+    return render(request, 'music/search.html', context)
