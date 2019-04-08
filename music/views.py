e2245beea7aae9b3457d74b00206cc958c171e6d
Paul Carroll <vpcarroll15@gmail.com>
2019-04-07 17:36:27 -0700

    Fix the album of the month logic

--- music/views.py
+++ music/views.py
@@ -4,9 +4,15 @@ from .models import Music
 
 
 def home(request):
+    """Renders the homepage for the music app."""
     recent_music = Music.objects.order_by('-created_at')[:10]
-
     context = {'recent_music': recent_music}
+
+    recommended_albums = Music.objects.filter(rating__gte=2)
+    if recommended_albums:
+        album_of_the_month = recommended_albums.order_by('-created_at')[0]
+        context['album_of_the_month'] = album_of_the_month
+
     return render(request, 'music/home.html', context)
 
 
