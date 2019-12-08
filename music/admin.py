ee17d6bbdd0fa687567c77c11724a53f49f8b900
Paul Carroll <vpcarroll15@gmail.com>
2019-12-08 15:01:49 -0800

    Allow us to manipulate BestOf objects in the Admin page

--- music/admin.py
+++ music/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Tag, Music, Musician
+from .models import Tag, Music, Musician, BestOf
 
 admin.site.register(Tag)
 admin.site.register(Music)
 admin.site.register(Musician)
+admin.site.register(BestOf)
