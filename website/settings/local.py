a3cf0f87f65817a2795ff60fa721adc6b305f6a6
Paul Carroll <vpcarroll15@gmail.com>
2020-05-11 21:00:13 -0700

    fix rds stuff

--- website/settings/local.py
+++ website/settings/local.py
@@ -3,13 +3,15 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
+# TODO: REVERT THIS BEFORE SQUASHING IN CHANGE!
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mywebsite',
-        'USER': 'paul',
-        'PASSWORD': os.environ['LOCAL_DB_PASSWORD'],
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     },
 }
+
