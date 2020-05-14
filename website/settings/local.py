1b9235408d0daa05080ede18041c5bc7ef76f6fb
Paul Carroll <vpcarroll15@gmail.com>
2020-05-13 20:55:08 -0700

    trying to switch settings back

--- website/settings/local.py
+++ website/settings/local.py
@@ -3,15 +3,13 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
-# TODO: REVERT THIS BEFORE SQUASHING IN CHANGE!
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['RDS_USERNAME'],
-        'PASSWORD': os.environ['RDS_PASSWORD'],
-        'HOST': os.environ['RDS_HOSTNAME'],
-        'PORT': os.environ['RDS_PORT'],
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mywebsite',
+        'USER': 'paul',
+        'PASSWORD': os.environ['LOCAL_DB_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '',
     },
 }
-
