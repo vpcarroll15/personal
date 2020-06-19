65de59772f72d3ecf6f7e97a78921fb892b8c30d
Paul Carroll <vpcarroll15@gmail.com>
2020-06-19 13:50:34 -0700

    add the summary page

--- scavenger_hunt/views.py
+++ scavenger_hunt/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import ScavengerHuntTemplate
 
-def index(request):
-    return HttpResponse("Wiring test.")
+
+def list_hunt_templates(request):
+    """Renders a view of all the scavenger hunt templates that can be instantiated."""
+    hunt_templates = ScavengerHuntTemplate.objects.all().order_by("-updated_at")
+    context = {'hunt_templates': hunt_templates}
+    return render(request, 'scavenger_hunt/hunt_templates.html', context)
--- website/settings/base.py
+++ website/settings/base.py
@@ -32,7 +32,11 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'music/templates/music')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'music/templates/music'),
+            os.path.join(BASE_DIR, 'scavenger_hunt/templates/scavenger_hunt'),
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
