85e4f78d3e8a505b57a57ddac0cb290346b899b3
Paul Carroll <vpcarroll15@gmail.com>
2020-06-19 14:29:10 -0700

    add detailed view

--- scavenger_hunt/views.py
+++ scavenger_hunt/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from .models import ScavengerHuntTemplate
 
@@ -6,5 +7,10 @@ from .models import ScavengerHuntTemplate
 def list_hunt_templates(request):
     """Renders a view of all the scavenger hunt templates that can be instantiated."""
     hunt_templates = ScavengerHuntTemplate.objects.all().order_by("-updated_at")
-    context = {'hunt_templates': hunt_templates}
-    return render(request, 'scavenger_hunt/hunt_templates.html', context)
+    return render(request, 'scavenger_hunt/hunt_templates.html', {'hunt_templates': hunt_templates})
+
+
+def hunt_template(request, hunt_template_id):
+    """Renders a single hunt template detailed view."""
+    hunt_template = get_object_or_404(ScavengerHuntTemplate, pk=hunt_template_id)
+    return render(request, 'scavenger_hunt/hunt_template.html', {'hunt': hunt_template})
