ce6c7a802d6c1296a0a724fe7a6205ea4f167a1a
Paul Carroll <vpcarroll15@gmail.com>
2020-11-15 12:05:18 -0800

    Add instruction to use basic auth for REST framework.

--- website/settings/base.py
+++ website/settings/base.py
@@ -30,6 +30,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ]
+}
+
 ROOT_URLCONF = 'website.urls'
 
 TEMPLATES = [
