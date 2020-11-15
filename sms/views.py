11487d999a3a54ee503dd024f9d7dd05697e2a8c
Paul Carroll <vpcarroll15@gmail.com>
2020-11-15 14:21:45 -0800

    restore important import

--- sms/views.py
+++ sms/views.py
@@ -2,6 +2,7 @@
 
 from django.http import HttpResponse
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from sms.permissions import UserInSmsManagerGroup, UserInSmsWebhookCaller
