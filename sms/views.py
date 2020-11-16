2ea3b58fcb052af9955cc405fe4f88bf350dccbf
Paul Carroll <vpcarroll15@gmail.com>
2020-11-16 08:22:58 -0800

    One small addition to faciliate debugging for Twilio stuff

--- sms/views.py
+++ sms/views.py
@@ -41,8 +41,10 @@ class WebhookView(SmsWebhookView):
         http POST sms/webhook/ {...(data)...}
         """
         # TODO: Replace this with something that isn't horrific.
+        with open("/tmp/testfile2.txt", "w") as testfile:
+            testfile.write(vars(request))
         with open("/tmp/testfile.txt", "w") as testfile:
-            testfile.write(request.data)
+            testfile.write(str(request.data))
         # Return a blank response to the webhook. We don't have any commands for Twilio
         # at this time, but we want it to know that everything worked as expected.
         return HttpResponse(status=204)
