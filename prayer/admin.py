dca2dff0992364170bb96aa40da92bbdd3d428b7
Paul Carroll <vpcarroll15@gmail.com>
2024-03-03 19:10:38 -0800

    Remove the field that makes the admin page load really slowly

--- prayer/admin.py
+++ prayer/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 from .models import PrayerSchema, PrayerSnippet
 
 
+class PrayerSnippetAdmin(admin.ModelAdmin):
+    # Exclude this because it loads way too many fields.
+    exclude = ["sms_data_point"]
+
+
+admin.site.register(PrayerSnippet, PrayerSnippetAdmin)
+
+
 admin.site.register(PrayerSchema)
-admin.site.register(PrayerSnippet)
