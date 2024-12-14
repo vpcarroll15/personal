58150a5abd034a615b8591ad8e0dbde11415a96c
Paul Carroll <vpcarroll15@gmail.com>
2024-12-13 20:11:24 -0800

    Sort musicians and tags in the admin page

--- music/admin.py
+++ music/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 
 from .models import Tag, Music, Musician, BestOf
 
-admin.site.register(Tag)
+
+class MusicianAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+class TagAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+admin.site.register(Tag, TagAdmin)
 admin.site.register(Music)
-admin.site.register(Musician)
+admin.site.register(Musician, MusicianAdmin)
 admin.site.register(BestOf)
