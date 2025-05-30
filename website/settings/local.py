67a609bb8a2856d11e88e665572b99e1eaae9d05
Paul Carroll <vpcarroll15@gmail.com>
2025-05-30 12:15:19 -0700

    Fix local database settings

--- website/settings/local.py
+++ website/settings/local.py
@@ -5,8 +5,8 @@ ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "mywebsite",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "ebdb",
         "USER": "paul",
         "PASSWORD": os.environ["LOCAL_DB_PASSWORD"],
         "HOST": "localhost",
